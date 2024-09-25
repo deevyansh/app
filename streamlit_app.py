279b1eee5cf3c41c539da2ce6ff90612f69b4900
Deevyansh Khadria <devkhadria@gmail.com>
2024-09-25 10:49:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,36 +5,36 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 login_page=st.Page(
-    page="pages/login.py",
+    page="Pages/login.py",
     title="Login",
     icon="👤",
     default=True
 )
 register_page=st.Page(
-    page="pages/register.py",
+    page="Pages/register.py",
     title="Register",
     icon="📊"
 )
 bidding_page = st.Page(
-    page="pages/bidding.py",
+    page="Pages/bidding.py",
     title="Make your bids",
     icon="💸"  # Money with wings, indicating bids or money transactions
 )
 
 check_dates = st.Page(
-    page="pages/check_dates.py",
+    page="Pages/check_dates.py",
     title="Check the Bidding hours",
     icon="⏰"  # Alarm clock, indicating time for checking bidding hours
 )
 
 result_page = st.Page(
-    page="pages/result.py",
+    page="Pages/result.py",
     title="Result Window",
     icon="🏆"  # Trophy, indicating results or success
 )
 
 logout=st.Page(
-    page="pages/logout.py",
+    page="Pages/logout.py",
     title="Logout"
 )
 
